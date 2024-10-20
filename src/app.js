3d1944f0b55df34373afc17c4abff39a0c40f762
Madhav <xybermadhav@gmail.com>
2024-10-20 23:09:39 +0530

    getting data from databases

--- src/app.js
+++ src/app.js
@@ -6,6 +6,41 @@ const User = require("./models/user")
 
 app.use(express.json());
 
+app.get("/user",async (req,res) => {
+    const userEmail = req.body.emailId;
+
+    try {
+        // agar hum yahan findOne ke me email Id ki jagah kuch na paas karein toh ye random user la ke de dega
+        const user = await User.findOne({emailId: userEmail});
+        if(!user)
+        {
+            res.status(404).send("User not found")
+        }
+        else{
+            console.log(user)
+            res.send(user);
+        }
+    } catch(err) {
+        res.status(400).send("Something went wrong")
+    }
+})
+
+app.get("/feed",async (req,res) => {
+    try{
+        const users = await User.find({emailId: req.body.emailId})
+
+        if(users.length === 0)
+        {
+            res.status(404).send("No user with this emailId found")
+        }
+        else{
+            res.send(users);
+        }
+    } catch(err) {
+        res.status(400).send("Something went wrong")
+    }
+})
+
 app.post("/signup" , async (req,res) => {
 
     
